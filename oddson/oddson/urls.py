37430610a91a93d4347410cfd53102bc202f79c7
George Marshall <george@socialups.com>
2012-08-23 16:35:44 -0700

    Added urls

--- oddson/oddson/urls.py
+++ oddson/oddson/urls.py
@@ -6,4 +6,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url('', include('number_match.urls')),
 )
