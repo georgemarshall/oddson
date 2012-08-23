c02c13d50db05ddc5b487e22b529ead416fd366d
George Marshall <george@socialups.com>
2012-08-23 16:16:07 -0700

    Stubbed out Contract REST views

--- oddson/number_match/views.py
+++ oddson/number_match/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from djangorestframework.views import ListModelView, ModelView
+
+from .resources import ContractResource
+
+
+class ContractListView(ListModelView):
+    resource = ContractResource
+
+
+class ContractView(ModelView):
+    resource = ContractResource
