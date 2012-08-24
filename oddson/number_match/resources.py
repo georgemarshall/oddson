bb5921584ca0f6967e8074b41ae7ff22ab4037e3
George Marshall <george@socialups.com>
2012-08-23 18:57:57 -0700

    Finished up number match REST view

--- oddson/number_match/resources.py
+++ oddson/number_match/resources.py
@@ -1,28 +1,28 @@
-from django.core.urlresolvers import reverse
 from djangorestframework.resources import ModelResource
+from djangorestframework.reverse import reverse
 
 from .models import Attempt, Contract
 
 
 class AttemptResource(ModelResource):
     exclude = ('id', 'pk', 'contract', 'ip_addr', 'request')
-    include = ('contract_uri', 'resource_uri', 'is_match')
+    include = ('contract', 'url', 'is_match')
     model = Attempt
 
-    def contract_uri(self, instance):
-        return reverse('number_match:contract_detail', kwargs={'id': instance.contract_id})
+    def contract(self, instance):
+        return reverse('number_match:contract_detail', kwargs={'id': instance.contract_id}, request=self.request)
 
-    def resource_uri(self, instance):
-        return reverse('number_match:attempt_detail', kwargs={'contract_id': instance.contract_id, 'id': instance.pk})
+    def url(self, instance):
+        return reverse('number_match:attempt_detail', kwargs={'contract_id': instance.contract_id, 'id': instance.pk}, request=self.request)
 
 
 class ContractResource(ModelResource):
     exclude = ('id', 'pk', 'creation_date', 'is_active')
-    include = ('attempt_uri', 'resource_uri')
+    include = ('attempts', 'url')
     model = Contract
 
-    def attempt_uri(self, instance):
-        return reverse('number_match:attempt_list', kwargs={'contract_id': instance.pk})
+    def attempts(self, instance):
+        return reverse('number_match:attempt_list', kwargs={'contract_id': instance.pk}, request=self.request)
 
-    def resource_uri(self, instance):
-        return reverse('number_match:contract_detail', kwargs={'id': instance.pk})
+    def url(self, instance):
+        return reverse('number_match:contract_detail', kwargs={'id': instance.pk}, request=self.request)
--- oddson/number_match/views.py
+++ oddson/number_match/views.py
@@ -1,29 +1,52 @@
 from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
+from djangorestframework import status
+from djangorestframework.mixins import PaginatorMixin, ReadModelMixin
+from djangorestframework.response import Response
 from djangorestframework.views import (
-    InstanceModelView, ListModelView, ListOrCreateModelView, View
+    ListModelView, ModelView, View
 )
 
+from .forms import AttemptForm
 from .resources import AttemptResource, ContractResource
 
 
-class AttemptListView(ListOrCreateModelView):
+class AttemptListView(PaginatorMixin, ListModelView):
+    form = AttemptForm
+    resource = AttemptResource
+
+    def post(self, request, *args, **kwargs):
+        contract = get_object_or_404(ContractResource.model, pk=kwargs['contract_id'])
+
+        instance = contract.attempt_set.create(
+            our_number=contract.generate_number(),
+            user_number=self.CONTENT['number'],
+            ip_addr=request.META.get('REMOTE_ADDR'),
+            request=dict(
+                (k.replace('HTTP_', '').replace('_', '-').title(), v)
+            for k, v in request.META.iteritems() if k.startswith('HTTP_'))
+        )
+
+        headers = {}
+        if hasattr(self.resource, 'url'):
+            headers['Location'] = self.resource(self).url(instance)
+        return Response(status.HTTP_201_CREATED, instance, headers)
+
+
+class AttemptView(ReadModelMixin, ModelView):
     resource = AttemptResource
 
 
-class AttemptView(InstanceModelView):
-    resource = AttemptResource
-
-
-class ContractListView(ListModelView):
+class ContractListView(PaginatorMixin, ListModelView):
     resource = ContractResource
 
 
-class ContractView(InstanceModelView):
+class ContractView(ReadModelMixin, ModelView):
     resource = ContractResource
 
 
 class Resources(View):
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         return [
             {'name': 'Contract API', 'resource_uri': reverse('number_match:contract_list')}
         ]
