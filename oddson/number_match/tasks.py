fefbe719ccc377ae049ef1412356a14efc54ebc9
George Marshall <george@socialups.com>
2012-08-24 14:57:02 -0700

    Added celery task to find expired games

---
+++ oddson/number_match/tasks.py
@@ -0,0 +1,31 @@
+from celery.task import task
+from django.utils import timezone
+
+from . import signals
+from .models import Game
+
+
+@task(name='number_match.tasks.end_expired_games')
+def end_expired_games():
+    """
+    Find all active games that have passed their expiration date
+    """
+    # Find expired games
+    games = Game.objects.filter(
+        is_active=True,
+        end_date__lte=timezone.now().date(),
+    )
+
+    # Deactivate the games
+    games.update(
+        is_active=False,
+        compleation_date=timezone.now(),
+    )
+
+    # Send game end signal
+    for game in games.iterator():
+        signals.game_has_ended.send(
+            sender=game.__class__,
+            game=game,
+            rules=['date']
+        )
